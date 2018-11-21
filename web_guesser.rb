e7e5a43705c4ae37e192c52323011e2715ba973b
Justin Mauldin <justin.mauldin7@gmail.com>
2018-11-21 16:59:40 -0700

    Adds way too high and low conditions.

--- web_guesser.rb
+++ web_guesser.rb
@@ -4,7 +4,11 @@ require 'sinatra/reloader'
 NUMBER = rand(100)
 
 def guess_check(string_guess)
-  if string_guess.to_i > NUMBER
+  if string_guess.to_i < NUMBER && (string_guess.to_i - NUMBER).abs > 5
+    "Way too low!"
+  elsif string_guess.to_i > NUMBER && (string_guess.to_i - NUMBER).abs > 5
+    "Way too high!"
+  elsif string_guess.to_i > NUMBER
     "Too high!"
   elsif string_guess.to_i < NUMBER
     "Too low!"
@@ -12,6 +16,8 @@ def guess_check(string_guess)
     "You are correct! The secret number is #{NUMBER}"
   end
 end
+
+
 
 get '/' do
   guess = params["guess"]
