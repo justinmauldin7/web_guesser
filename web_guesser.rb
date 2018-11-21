4d532d44c93949a8f662b864902485271823377a
Justin Mauldin <justin.mauldin7@gmail.com>
2018-11-21 16:48:25 -0700

    Adds guess_check method to check guess and adds output for too high or too low.

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,7 +1,20 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
+NUMBER = rand(100)
+
+def guess_check(string_guess)
+  if string_guess.to_i > NUMBER
+    "Too high!"
+  elsif string_guess.to_i < NUMBER
+    "Too low!"
+  elsif string_guess.to_i == NUMBER
+    "You are correct! The secret number is #{NUMBER}"
+  end
+end
+
 get '/' do
-  number = rand(100)
-  erb :index, :locals => {:number => number}
+  guess = params["guess"]
+  message = guess_check(guess)
+  erb :index, :locals => {:number => NUMBER, :message => message}
 end
